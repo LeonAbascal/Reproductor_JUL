43e79d6608a91709262c6cd5cd80abe4341def52
León Abascal <leonabascal@opendeusto.es>
2020-10-13 23:28:03 +0200

    Creada clase de ventana principal y añadidos elementos básicos.

---
+++ Reproductor_JUL/src/reproductor/VentanaPrincipal.java
@@ -0,0 +1,27 @@
+package reproductor;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class VentanaPrincipal extends JFrame {
+	
+	public VentanaPrincipal() {
+		
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(600, 400);
+		setResizable(false);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				new VentanaPrincipal();
+				
+			}
+		});
+	}
+}
