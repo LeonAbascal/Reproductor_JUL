4a81d1a1a5fc3d134ab094f4d2d3748bd040aaec
León Abascal <leonabascal@opendeusto.es>
2020-10-28 17:22:44 +0100

    Main window created.

---
+++ Reproductor_JUL/src/reproductor/MainWindow.java
@@ -0,0 +1,18 @@
+package reproductor;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class MainWindow extends JFrame {
+	
+	public MainWindow() {
+		
+		setTitle("Título");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(600, 400);
+		setResizable(false);
+		setVisible(true);
+		
+	}
+
+}
