3be0e46aaddcfd23fa3c58db443e6a9fac15c7e7
León Abascal <leonabascal@opendeusto.es>
2020-11-05 12:41:56 +0100

    Basic window layout design

--- Reproductor_JUL/src/reproductor/windows/MainWindow.java
+++ Reproductor_JUL/src/reproductor/windows/MainWindow.java
@@ -1,27 +1,100 @@
 package reproductor.windows;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
+import javax.swing.border.Border;
 
 public class MainWindow extends JFrame {
-	
-	JButton b = new JButton("");
-	
-	
-	public MainWindow() {
 
-		add(b);
+	JMenuBar menuBar;
+		JMenu fileMenu;
+			JMenuItem openMp3MU;
+			JMenuItem saveMp3MU;
+			
+		JMenu playlistMenu;
+			JMenuItem newPlaylist;
+			JMenuItem openPlaylist;
+			JMenuItem savePlaylist;
+			JMenuItem queue;
+	
+	
+	JPanel centerPanel; // border layout
+		JPanel imagePanel; // to the left
+		JPanel metadataPanel; // to the right	
+			JLabel metadataLabel;
+			JLabel titleLabel;
+			JLabel artistLabel;
+			JLabel albumLabel;
+			
+	JPanel southPanel;
+		JButton playB;
+		JButton pauseB;
+		JButton randomB;
+		JButton previousB;
+		JButton nextB;
+		
+	public MainWindow() {
+		guiComponentDeclaration();
+		addComponentsToWindow();
+		
 		setTitle("Título");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(600, 400);
 		setResizable(false);
 		setVisible(true);
+	}
+	
+	private void guiComponentDeclaration() {
+		// PANELS
+		centerPanel = new JPanel(new BorderLayout());
+		southPanel = new JPanel(new FlowLayout());
+		imagePanel = new JPanel();
+		metadataPanel = new JPanel(new GridLayout());
+		
+		// MENU BAR
+		fileMenu = new JMenu("File");
+		playlistMenu = new JMenu("Playlist");
+		openMp3MU = new JMenuItem("Open MP3 file");
+		saveMp3MU = new JMenuItem("Save MP3");
+		
+		newPlaylist = new JMenuItem("New...");
+		openPlaylist = new JMenuItem("Open...");
+		savePlaylist = new JMenuItem("Save...");
+		queue = new JMenuItem("Queue");
+		
+		// THESE BUTTONS WILL CONTAIN IMAGES
+		playB = new JButton("Play");
+		pauseB = new JButton("Pause");
+		randomB = new JButton("Random");
+		previousB = new JButton("Previous");
+		nextB = new JButton("Next");
+	}
+	
+	private void addComponentsToWindow() {
+		southPanel.add(previousB);
+		southPanel.add(playB);
+		southPanel.add(nextB);
+		southPanel.add(randomB);
+		
+		centerPanel.add(metadataPanel, BorderLayout.EAST);
+		centerPanel.add(imagePanel, BorderLayout.WEST);
+		
+		add(centerPanel, BorderLayout.CENTER);
+		add(southPanel, BorderLayout.SOUTH);
 	}
 	
 	private static void applySkin() {
