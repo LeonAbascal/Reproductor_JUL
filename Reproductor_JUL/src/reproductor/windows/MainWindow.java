849b2369ec4cc36aab0c84d9494c979d54617291
UnaiCL <unai.calegain@opendeusto.es>
2020-12-22 10:59:09 +0100

    MainWindow now supports LogIn

--- Reproductor_JUL/src/reproductor/windows/MainWindow.java
+++ Reproductor_JUL/src/reproductor/windows/MainWindow.java
@@ -29,29 +29,28 @@ import javax.swing.ImageIcon;
 public class MainWindow extends JFrame {
 
 	private static Logger logger = Logger.getLogger(MainWindow.class.getName());
-	
+
 	JMenuBar menuBar;
 		JMenu fileMenu;
 			JMenuItem openMp3MU;
 			JMenuItem saveMp3MU;
-			
+
 		JMenu playlistMenu;
 			JMenuItem newPlaylist;
 			JMenuItem openPlaylist;
 			JMenuItem savePlaylist;
 			JMenuItem queue;
-	
-	
+
 	JPanel centerPanel; // border layout
 		JPanel menuPanel; // to the left
 			JLabel imageText;
-		JPanel metadataPanel; // to the right	
+		JPanel metadataPanel; // to the right
 			JLabel metadataLabel;
 			JLabel titleLabel;
 			JLabel artistLabel;
 			JLabel albumLabel;
 		JPanel songsPanel;
-			
+
 	JPanel southPanel;
 		JButton playB;
 		JButton pauseB;
@@ -59,23 +58,26 @@ public class MainWindow extends JFrame {
 		JButton previousB;
 		JButton nextB;
 		
-		// Generados por WindowBuilder
-		private JLabel metadataText;
-		private JLabel txtMenuDescendente;
-		private JLabel txtSongName;
-		
+	static LogInWindow login_w;
+
+	// Generados por WindowBuilder
+	private JLabel metadataText;
+	private JLabel txtMenuDescendente;
+	private JLabel txtSongName;
+
 	public MainWindow() {
 		guiComponentDeclaration();
 		addComponentsToWindow();
-		
+
 		setTitle("Título");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(720, 480);
 		setResizable(false);
 		setVisible(true);
+		setLocationRelativeTo(null);
 		logger.log(Level.SEVERE, "logger de prueba");
 	}
-	
+
 	private void guiComponentDeclaration() {
 		// PANELS
 		centerPanel = new JPanel(new BorderLayout());
@@ -83,61 +85,61 @@ public class MainWindow extends JFrame {
 		menuPanel = new JPanel();
 		metadataPanel = new JPanel(new GridLayout());
 		songsPanel = new JPanel(null);
-		
+
 		menuPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-		
+
 		txtMenuDescendente = new JLabel("Men\u00FA Descendente");
 		menuPanel.add(txtMenuDescendente);
 		metadataPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-		
+
 		metadataText = new JLabel("Metadata");
 		metadataPanel.add(metadataText);
-		
+
 		// MENU BAR
 		fileMenu = new JMenu("File");
 		playlistMenu = new JMenu("Playlist");
 		openMp3MU = new JMenuItem("Open MP3 file");
 		saveMp3MU = new JMenuItem("Save MP3");
-		
+
 		newPlaylist = new JMenuItem("New...");
 		openPlaylist = new JMenuItem("Open...");
 		savePlaylist = new JMenuItem("Save...");
 		queue = new JMenuItem("Queue");
-		
+
 		// THESE BUTTONS WILL CONTAIN IMAGES
 		playB = new JButton("Play");
 		pauseB = new JButton("Pause");
 		randomB = new JButton("Random");
 		previousB = new JButton("Previous");
 		nextB = new JButton("Next");
-		
+
 		// Songs icons + text generator
 		JButton pruebaBoton = new JButton("");
 		pruebaBoton.setIcon(new ImageIcon("MusicFiles/Icons/DoraemonBumpingRemix.png"));
 		pruebaBoton.setBounds(10, 10, 96, 82);
 		songsPanel.add(pruebaBoton);
-				
+
 		txtSongName = new JLabel("Song name");
 		txtSongName.setHorizontalAlignment(SwingConstants.CENTER);
 		txtSongName.setBounds(10, 102, 96, 18);
 	}
-	
+
 	private void addComponentsToWindow() {
 		southPanel.add(previousB);
 		southPanel.add(playB);
 		southPanel.add(nextB);
 		southPanel.add(randomB);
-		
+
 		centerPanel.add(metadataPanel, BorderLayout.EAST);
 		centerPanel.add(menuPanel, BorderLayout.WEST);
 		centerPanel.add(songsPanel, BorderLayout.CENTER);
-		
+
 		getContentPane().add(centerPanel, BorderLayout.CENTER);
-		
+
 		songsPanel.add(txtSongName);
 		getContentPane().add(southPanel, BorderLayout.SOUTH);
 	}
-	
+
 	private static void applySkin() {
 		try {
 			Scanner sc = new Scanner(new FileInputStream("JUL.init"));
@@ -145,47 +147,51 @@ public class MainWindow extends JFrame {
 			switch (skinLine) {
 			case "skin=SWING": {
 				UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-				//logger.log(Level.INFO, "First line read: " + skinLine + " in lionSynth.init. Default skin applied.");
+				// logger.log(Level.INFO, "First line read: " + skinLine + " in lionSynth.init.
+				// Default skin applied.");
 				break;
 			}
 			case "skin=MOTIF": {
 				UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
-				//logger.log(Level.INFO, "First line read: " + skinLine + " in lionSynth.ini. Motif skin applied.");
+				// logger.log(Level.INFO, "First line read: " + skinLine + " in lionSynth.ini.
+				// Motif skin applied.");
 				break;
 			}
 			case "skin=NIMBUS": {
 				UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
 				System.out.println("Classic Macintosh skin applied");
-				//logger.log(Level.INFO, "First line read: " + skinLine + " in lionSynth.ini. Classic Machintosh skin applied");
+				// logger.log(Level.INFO, "First line read: " + skinLine + " in lionSynth.ini.
+				// Classic Machintosh skin applied");
 				break;
 			}
 			case "skin=WINDOWS": {
 				UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-				//logger.log(Level.INFO, "First line read: " + skinLine + " in lionSynth.ini. Windows skin applied");
+				// logger.log(Level.INFO, "First line read: " + skinLine + " in lionSynth.ini.
+				// Windows skin applied");
 			}
 			default:
 				System.out.println("Not valid value in init file: " + skinLine);
 			}
-			
+
 			sc.close();
-			
+
 		} catch (FileNotFoundException e) {
-			//logger.log(Level.FINE, "lionSynth.init file was deleted or does not still exist. Skin won't be applied.");
+			// logger.log(Level.FINE, "lionSynth.init file was deleted or does not still
+			// exist. Skin won't be applied.");
 		} catch (Exception e) {
 			System.err.println("Skin could not be applied.");
-			//logger.log(Level.WARNING, "Skin could not be applied.");
+			// logger.log(Level.WARNING, "Skin could not be applied.");
 		}
 	}
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 
-			
-			
 			@Override
 			public void run() {
 				applySkin();
-				new LogInWindow();
+				login_w = new LogInWindow();
+				login_w.setVisible(true);
 			}
 		});
 	}
