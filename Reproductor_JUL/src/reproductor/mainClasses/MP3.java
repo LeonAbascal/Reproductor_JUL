77f3a385cd592e6828c263a0365eba47f16867f9
León Abascal <leonabascal@opendeusto.es>
2020-12-22 20:24:30 +0100

    Revert "MP3 playback class updated"
    This reverts commit a85c921b626e10cd7ebc1fe31c2ed3af0ecbe7bf.

--- Reproductor_JUL/src/reproductor/mainClasses/MP3.java
+++ Reproductor_JUL/src/reproductor/mainClasses/MP3.java
@@ -1,46 +1,72 @@
 package reproductor.mainClasses;
 
-import java.io.File;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 
-import javazoom.jlgui.basicplayer.BasicPlayer;
+import javazoom.jl.player.Player;
 
 
 public class MP3 {
-    private static BasicPlayer player = new BasicPlayer();
+    private String filename;
+    private Player player; 
 
-    public static void play(File f) {
+    // constructor that takes the name of an MP3 file
+    public MP3(String filename) {
+        this.filename = filename;
+    }
+
+    public void close() { if (player != null) player.close(); }
+
+    // play the MP3 file to the sound card
+    public void play() {
         try {
-        	player.open(f);
-        	player.play();
+            FileInputStream fis     = new FileInputStream(filename);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            player = new Player(bis);
         }
         catch (Exception e) {
-            System.out.println("Problem playing file " + f);
+            System.out.println("Problem playing file " + filename);
             System.out.println(e);
         }
+
+        // run in new thread to play in background
+        new Thread() {
+            public void run() {
+                try { player.play(); }
+                catch (Exception e) { System.out.println(e); }
+            }
+        }.start();
+
+
+
+
     }
-    
-    public static void pause() {
-    	try {
-    		player.pause();
-    	} catch (Exception e) {
-    		
-    	}
-    }
-    
-    public static void stop() {
-    	try {
-    		player.stop();
-    	} catch (Exception e) {
-    		System.err.println("No se pudo detener el reproductor.");
-    	}
-    }
-    
-    public static void resume() {
-    	try {
-    		player.resume();
-    	} catch (Exception e) {
-    		
-    	}
+
+
+    // test client
+    public static void main(String[] args) {
+        String filename = "MusicFiles\\Beave - Too Much Ft. Bethany Lamb (Torin Dundas Remix).mp3";
+        MP3 mp3 = new MP3(filename);
+        mp3.play();
+
+        // do whatever computation you like, while music plays
+        /*
+        int N = 4000;
+        double sum = 0.0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sum += Math.sin(i + j);
+            }
+        }
+        System.out.println(sum);
+		*/
+        // when the computation is done, stop playing it
+        mp3.close();
+
+        // play from the beginning
+        mp3 = new MP3(filename);
+        mp3.play();
+
     }
 
 }
