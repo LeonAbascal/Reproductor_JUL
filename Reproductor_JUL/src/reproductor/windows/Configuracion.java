0834b0f941b1dfacd7d1f3b5ed64e66918bf4864
Julen396 <julen396@gmail.com>
2020-12-23 13:29:53 +0100

    Configuration window

---
+++ Reproductor_JUL/src/reproductor/windows/Configuracion.java
@@ -0,0 +1,87 @@
+package reproductor.windows;
+
+import java.awt.BorderLayout;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Properties;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.ComboBoxModel;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.DefaultListModel;
+import javax.swing.JComboBox;
+import javax.swing.JList;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.ListSelectionModel;
+import javax.swing.SwingUtilities;
+import javax.swing.border.Border;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+public class Configuracion extends JFrame {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    public Configuracion() {
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setTitle("Configuracion");
+        setVisible(true);
+        setSize(200, 200);
+
+        // datos de ejemplo
+        String[] strings = { 
+        		"SWING", 
+        		"MOTIF",
+        		"NIMBUS",
+        		"INDOWS"
+        };
+
+        ComboBoxModel<String> comboBoxModel = new DefaultComboBoxModel<>(strings);
+        JComboBox<String> comboBox = new JComboBox<>(comboBoxModel);
+
+        JPanel comboBoxPanel = new JPanel();
+        Border comboBoxPanelBorder = BorderFactory.createTitledBorder("JComboBox");
+        comboBoxPanel.setBorder(comboBoxPanelBorder);
+        comboBoxPanel.add(comboBox);
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        mainPanel.add(comboBoxPanel);
+        add(mainPanel, BorderLayout.CENTER);
+        
+        comboBox.addItemListener(new ItemListener() {
+
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                // se comprueba si se ha seleccionado o deseleccionado un elemento de la lista
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                	try (FileReader reader = new FileReader("/Info Files/propertiesproperties")) {
+                        // Se crea el objeto y se leen las propiedades del fichero
+                        Properties properties = new Properties();
+                        properties.load(reader);
+                        
+                        // Se puede acceder al valor de las propiedades por nombre
+                        properties.setProperty("skin", (String) comboBox.getSelectedItem());
+                        
+                        
+
+                    } catch (IOException e1) {
+                        System.out.println("No se pudo leer el fichero de propiedades");
+                    }
+                }
+
+                if (e.getStateChange() == ItemEvent.DESELECTED) {
+                    System.out.println("Deseleccionado: " + e.getItem());
+                }
+            }
+
+        });
+    }
+}
