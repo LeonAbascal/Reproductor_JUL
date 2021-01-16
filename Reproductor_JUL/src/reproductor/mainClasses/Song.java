da9bdd3a67117dff50fed8368aa452fee54668fd
León Abascal <leonabascal@opendeusto.es>
2021-01-16 17:37:49 +0100

    Song properties added (genre, year)

--- Reproductor_JUL/src/reproductor/mainClasses/Song.java
+++ Reproductor_JUL/src/reproductor/mainClasses/Song.java
@@ -6,14 +6,18 @@ public class Song {
 	private String album;
 	private String track;
 	private String path;
+	private String genre;
+	private String year;
 	
-	public Song(String name, String artist, String album, String track, String duration, String path) {
+	public Song(String name, String artist, String album, String track, String duration, String path, String genre, String year) {
 		super();
 		this.name = name;
 		this.artist = artist;
 		this.album = album;
 		this.track = track;
-		this.path=path;
+		this.path = path;
+		this.genre = genre;
+		this.year = year;
 	}
 
 	public Song() {
@@ -23,6 +27,8 @@ public class Song {
 		this.album = "";
 		this.track = "";
 		this.path="";
+		this.year = "";
+		this.genre = "";
 	}
 
 	public String getName() {
@@ -63,13 +69,33 @@ public class Song {
 	public void setPath(String path) {
 		this.path = path;
 	}
+	
+	public void setYear(String year) {
+		this.year = year;
+	}
+	
+	public String getYear() {
+		return year;
+	}
+	
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	
+	public String getGenre() {
+		return genre;
+	}
+	
+	
+	
+	
 	@Override
     public boolean equals(Object o) {
         if (!(o instanceof Song))
             return false;
 
         Song s = (Song) o;
-        return s.name.equals(this.name);
+        return s.path.equals(this.path);
     }
 	
 }
